a6d9aeac18fcf91226ac0c10dd606921793cf2b0
Shaleen Jain <shaleen@jain.sh>
2019-12-20 17:24:10 +0530

    refactor to remove recursion and use a Node struct

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,90 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-#[derive(Debug)]
-struct Graph {
-    pub nodes: Vec<u32>,
-    pub edges: HashMap<u32, Vec<u32>>,
+#[derive(Debug, Hash, Eq, PartialEq)]
+struct Node {
+    id: u32,
+    //    visited: bool, // should not be used to compare.
 }
 
-impl Graph {
-    fn sort(&self, node: u32, visited: &mut HashSet<u32>, result: &mut Vec<u32>) {
-        visited.insert(node);
+impl Node {
+    fn new(id: u32) -> Node {
+        Node { id }
+    }
+}
 
-        for dep in &self.edges[&node] {
-            if !visited.contains(dep) {
-                self.sort(*dep, visited, result)
+#[derive(Debug)]
+struct Graph<'a> {
+    pub nodes: Vec<&'a Node>,
+    pub edges: HashMap<&'a Node, Vec<&'a Node>>,
+}
+
+impl<'a> Graph<'a> {
+    fn sort(&self) -> Vec<&Node> {
+        let mut visited = HashSet::new();
+        let mut result = Vec::with_capacity(self.nodes.len());
+
+        // Make sure we traverse all nodes;
+        for node in &self.nodes {
+            // don't re-visit a node.
+            if !visited.contains(node) {
+                let mut stack = Vec::new();
+                stack.push((*node, false));
+
+                while !stack.is_empty() {
+                    let (cur_node, is_parent) = stack.pop().unwrap();
+
+                    // if we get back a parent node then we have
+                    // already visited all child nodes.
+                    if is_parent {
+                        result.push(cur_node);
+                        continue;
+                    }
+
+                    visited.insert(cur_node);
+                    stack.push((cur_node, true));
+
+                    if self.edges.get(&cur_node).is_none() {
+                        continue;
+                    }
+
+                    for dep in &self.edges[&cur_node] {
+                        if !visited.contains(dep) {
+                            stack.push((*dep, false));
+                        }
+                    }
+                }
             }
         }
-        // if we exit from the loop it means we have already visited all deps.
-        result.push(node);
+
+        result
     }
 }
 
 fn main() {
-    let nodes = vec![5, 7, 3, 8, 11, 2, 9, 10];
+    let (_5, _7, _3, _8, _11, _2, _9, _10) = (
+        Node::new(5),
+        Node::new(7),
+        Node::new(3),
+        Node::new(8),
+        Node::new(11),
+        Node::new(2),
+        Node::new(9),
+        Node::new(10),
+    );
+    let nodes = vec![&_5, &_7, &_3, &_8, &_11, &_2, &_9, &_10];
     let mut edges = HashMap::new();
-    edges.insert(5, vec![11]);
-    edges.insert(7, vec![11, 8]);
-    edges.insert(3, vec![10, 8]);
-    edges.insert(11, vec![2, 9, 10]);
-    edges.insert(8, vec![9]);
-    edges.insert(2, vec![]);
-    edges.insert(9, vec![]);
-    edges.insert(10, vec![]);
-
-    let mut visited = HashSet::new();
-    let mut result = Vec::with_capacity(nodes.len());
+    edges.insert(&_5, vec![&_11]);
+    edges.insert(&_7, vec![&_11, &_8]);
+    edges.insert(&_3, vec![&_10, &_8]);
+    edges.insert(&_11, vec![&_2, &_9, &_10]);
+    edges.insert(&_8, vec![&_9]);
     let graph = Graph { nodes, edges };
 
-    // Make sure we traverse all nodes;
-    for node in &graph.nodes {
-        // don't re-visit a node.
-        if !visited.contains(node) {
-            graph.sort(*node, &mut visited, &mut result);
-        }
-    }
+    let result = graph.sort();
 
     for name in result.iter().rev() {
-        print!("{}, ", name);
+        print!("{}, ", name.id);
     }
 }
 
@@ -55,31 +93,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_sorts_correctly() {
-        let nodes = vec![5, 7, 3, 8, 11, 2, 9, 10];
-        let mut edges = HashMap::new();
-        edges.insert(5, vec![11]);
-        edges.insert(7, vec![11, 8]);
-        edges.insert(3, vec![10, 8]);
-        edges.insert(11, vec![2, 9, 10]);
-        edges.insert(8, vec![9]);
-        edges.insert(2, vec![]);
-        edges.insert(9, vec![]);
-        edges.insert(10, vec![]);
-
-        let mut visited = HashSet::new();
-        let mut result = Vec::with_capacity(nodes.len());
-        let graph = Graph { nodes, edges };
-
-        // Make sure we traverse all nodes;
-        for node in &graph.nodes {
-            // don't re-visit a node.
-            if !visited.contains(node) {
-                graph.sort(*node, &mut visited, &mut result);
-            }
-        }
-
-        result.reverse(); // LIFO order since we treat result as a stack.
-        assert_eq!(result, [3, 7, 8, 5, 11, 10, 9, 2,]);
-    }
+    fn it_sorts_correctly() {}
 }
